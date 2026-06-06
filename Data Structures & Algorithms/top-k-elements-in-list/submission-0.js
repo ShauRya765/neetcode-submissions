355b7cfbf1dd7eaf36c5d0910de8ce3265e65447
ShauRya765 <73055904+ShauRya765@users.noreply.github.com>
2026-06-06 16:05:59 -0400

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const temp = {}
+
+        for (const i of nums) {
+            temp[i] = (temp[i] ?? 0) + 1;
+        }
+        return Object.keys(temp).sort((a,b)=>temp[b]-temp[a]).splice(0,k).map(Number);
+    }
+}

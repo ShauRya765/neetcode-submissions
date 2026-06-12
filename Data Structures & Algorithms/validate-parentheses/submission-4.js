30cabce3a3a05122fe6e8304642a9a90029283e2
ShauRya765 <73055904+ShauRya765@users.noreply.github.com>
2026-06-12 13:52:38 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const len = s.length;
+        if (len < 2) {
+            return false;
+        }
+        const stack = []
+        const pairs = { ')': '(', '}': '{', ']': '[' }
+
+        for (const char of s) {
+            if (char in pairs) {
+                if (stack[stack.length - 1] === pairs[char]) {
+                    stack.pop()
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(char);
+            }
+        }
+        return stack.length === 0
+    }
+}

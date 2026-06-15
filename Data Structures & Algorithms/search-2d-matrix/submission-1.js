7ce86563175a31d22b9730d531a0facb188e561e
ShauRya765 <73055904+ShauRya765@users.noreply.github.com>
2026-06-15 17:18:56 -0400

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+
+    // key in this logic, is to conver the array into a 1D array, or treat it.
+    searchMatrix(matrix, target) {
+        if (matrix.length === 0) {
+            return false
+        }
+        let m = matrix.length;
+        let n = matrix[0].length;
+
+        let left = 0;
+        let right = m * n - 1; 
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            let row = Math.floor(mid / n);
+            let col = mid % n;
+            if (matrix[row][col] === target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+
+    }
+}
